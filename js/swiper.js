ae1e9738982cdd2fea1e640754ea6e875b9f84ee
RomaMirzo <RomaMirzo@users.noreply.github.com>
2020-02-10 18:27:49 +0200

    add skills, about section and other

--- js/swiper.js
+++ js/swiper.js
@@ -1,9 +1,15 @@
-// var swiper = new Swiper('.swiper-container', {
-//     slidesPerView: 3,
-//     slidesPerColumn: 2,
-//     spaceBetween: 30,
-//     pagination: {
-//       el: '.swiper-pagination',
-//       clickable: true,
-//     },
-//   });
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    slidesPerColumn: 2,
+    spaceBetween: 1,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+  var swiper1 = new Swiper('.swiper-container1', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
